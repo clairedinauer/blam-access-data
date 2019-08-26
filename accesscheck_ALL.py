206e0a3d6d2efaf6794d203b54dd600231a295c7
Joshua Kenney <joshuagkenney@gmail.com>
2019-08-26 13:59:08 -0400

    ok

--- accesscheck_ALL.py
+++ accesscheck_ALL.py
@@ -1,21 +1,21 @@
 import pandas as pd
 import numpy as np
 import predCode  # predictive coding participant list
-import predCode_access_db # predictive coding access checklist
+import predCode_access_db  # predictive coding access checklist
 
-#Link to the practice_accesscheck.xlsx Excel sheet
-#This sheet covers the "Access" data entry checklist
+# Link to the practice_accesscheck.xlsx Excel sheet
+# This sheet covers the "Access" data entry checklist
 
 print("This system will indicate if a participant's forms have been entered in Access.")
-df = pd.read_excel(r'practice_accesscheck.xlsx', sheet_name = 'ACCESS')
+df = pd.read_excel(r'practice_accesscheck.xlsx', sheet_name='ACCESS')
 
 for beliefid in predCode.belieflist:
-    print ("\nParticipant " + beliefid + " is missing:")
+    print("\nParticipant " + beliefid + " is missing:")
 
-    #this selects all rows where the participant id is equal to the user input
+    # this selects all rows where the participant id is equal to the user input
     df_temp = df[df['MPRCID'] == beliefid]
 
-    #this resets the index to regular, so we can just use iloc(0)
+    # this resets the index to regular, so we can just use iloc(0)
     df_temp = df_temp.reset_index()
 
     for i in predCode_access_db.forms:
