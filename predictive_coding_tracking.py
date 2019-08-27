6aff4499de1b898918e9df3f439a3c9d9507d255
Claire Dinauer <cdinauer@g.ucla.edu>
2019-08-27 15:11:38 -0400

    Created predictive_coding_tracking.py and edited tableEvents_ALL.py

--- predictive_coding_tracking.py
+++ predictive_coding_tracking.py
@@ -15,10 +15,33 @@ def select():
     print("1. Lookup specific participant ID.")
     print("2. List all missing data from all subjects.")
     print("3. Add new subject to database.")
-    print("4. Remove subject from database.\n")
+    print("4. Remove subject from database.")
+    print("5. Bye!\n")
     global selection
     selection = int(input("What would you like to do? "))
     s(1)
+
+
+def lookup():
+    beliefid = str.upper((input('Please enter BeliefID: ')))
+    if beliefid in predCode_participant_list.belieflist:
+        print("\nBeliefID found. Here is what I have on file for " + beliefid + ":")
+
+        # this selects all rows where the participant id is equal to the user input
+        df_temp = df[df['MPRCID'] == beliefid]
+
+        # this resets the index to regular, so we can just use iloc(0)
+        df_temp = df_temp.reset_index()
+
+        for i in predCode_table_of_events.toeForms:
+            if df_temp.loc[0, i] == 'no':
+                print(i + ' missing')
+
+        print('NOTES:')
+        print(df.loc[df.MPRCID == beliefid, 'NOTES'])
+
+    else:
+        print("Sorry, I do not have " + beliefid + " on file.")
 
 
 def listall():
@@ -39,6 +62,10 @@ def listall():
         print(df.loc[df.MPRCID == beliefid, 'NOTES'])
 
 
+def add(a):
+    beliefid = str.upper((input('Please enter BeliefID: ')))
+
+
 if __name__ == "__main__":
     select()
     if selection == 1:
@@ -49,3 +76,7 @@ if __name__ == "__main__":
         add()
     elif selection == 4:
         remove()
+    elif selection == 5:
+        print("Until next time, my friend :)")
+        s(2)
+        exit()
